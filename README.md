cb7b8bf5b16aa0d1f19de8026f0184b72a4b94bd
江仑 <jianglun.jl@alibaba-inc.com>
2015-11-22 20:16:08 +0800

    fix bug

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-### 必须放在postloader
+# Introduction
+
+loader-chinese2unicode is webpack loader , the main purpose is convert chinese character to unicode character, to suit all of different encoding web pages.
+
+
+# useage
+
+``` 
+ postLoaders: [   
+      { test: /\.js/, loader: "@ali/loader-chinese2unicode" }  
+    ]  
+```
+
--- index.js
+++ index.js
@@ -4,8 +4,6 @@ var fs = require('fs');
 var jschardet = require('jschardet');
 var iconv = require('iconv-lite');
 
-
-
 function toUnicode(s){
   return s.replace(/([\u4E00-\u9FA5]|[\uFE30-\uFFA0])/g, function(){
     return '\\u' + RegExp['$1'].charCodeAt(0).toString(16);
@@ -16,7 +14,7 @@ module.exports = function ( content ) {
   var self = this;
   var filePath = this.resourcePath;
   var filebuffer = fs.readFileSync(filePath);
-
+  var encoding = this.query.replace('?','');
   var fileEncoding = encoding ?
     encoding :
     jschardet.detect(filebuffer).encoding;
@@ -27,12 +25,12 @@ module.exports = function ( content ) {
 
   if( !iconv.encodingExists(fileEncoding) ) {
     console.log(('[WARN]').yellow, 'file:', filePath, 'encoding is', (fileEncoding).red, 'but iconv-lite not support it!');
+    return content;
   }
 
   var fileStr = iconv.decode(filebuffer, fileEncoding);
 
   return toUnicode(fileStr);
-
 
 };
 
